3e241e19f527f97d571e02208aeea5994e1494f4
akshada8102 <76065536+akshada8102@users.noreply.github.com>
2025-04-06 00:50:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(cookieParser());
+
+// Set cookie across all subdomains of example.com
+app.get("/set-cookie", (req, res) => {
+  res.cookie("token", "sainapints toke", {
+    // domain: ".example.com", // enables sharing across subdomains
+    path: "/",
+    httpOnly: true,
+    secure: true, // set to true if using HTTPS
+    sameSite: "lax", // or 'none' if using cross-site requests
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  });
+  res.send("Cookie has been set across subdomains!");
+});
+
+// Access cookie
+app.get("/get-cookie", (req, res) => {
+  const token = req.cookies.token;
+  res.send(`Token from cookie: ${token}`);
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Server running on port ::"+PORT);
+});
